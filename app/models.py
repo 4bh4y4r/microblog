ecde1849c71cb694279e7d06d608f83972f10caf
Abhay <abhayachuthanr11@gmail.com>
2026-06-24 13:54:33 +0530

    Further authentication implemented, logs in user, next to create login and registration forms

---
+++ app/models.py
@@ -0,0 +1,46 @@
+from app import db 
+from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+
+class User(UserMixin,db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), nullable = False, unique = True)
+    password_hash = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+
+    posts = db.relationship("Post", backref = "author")
+
+    def __repr__(self):
+        return f"User {self.username}"
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return self.check_password_hash(self.password_hash, password)
+    
+
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(400), nullable = False)
+    timestamp = db.Column(db.DateTime, default = lambda : datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post {self.body[:20]}"
+    
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for,flash
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User, Post
 
 
 @app.route('/index')
@@ -11,9 +13,21 @@ def index():
 
 @app.route('/login', methods = ["GET", "POST"])
 def login():
-
+    if current_user.is_authenticated:
+        return redirect(url_for('index.html'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"login requested for {form.username.data}, remember_me = {form.remember_me.data}")
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid credentials")
+            return redirect(url_for('login'))
+        
+        login_user(user, remember_me = form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', form = form, title = "Log In")
+
+
+@app.route('/logout')
+def logout():
+    logout_user
+    return redirect(url_for('login'))
